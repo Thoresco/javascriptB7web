a2e3a661651189c05aea9cb646a332c535f0988b
Rivaldo Monte Alegre Jr <46612260+Thoresco@users.noreply.github.com>
2024-11-20 21:06:31 -0300

    Add files via upload

---
+++ JS/javascriptB7web/README.md
@@ -0,0 +1,1 @@
+# javascriptB7web
---
+++ JS/javascriptB7web/assets/js/script.js
@@ -0,0 +1,9 @@
+let nome = "riva"
+let idade = 42
+let cidade = "Guarujá"
+let estado = "SP"
+
+console.log(idade)
+console.log (nome)
+console.log (cidade)
+console.log (estado)
